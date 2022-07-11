32fc6b9406f3ace93416d88236374883f917a6fb
Lina Belitova <libra28@ya.ru>
2022-07-11 18:49:31 +0300

    Refactor: js code

--- script/index.js
+++ script/index.js
@@ -1,5 +1,7 @@
-const profileForm = document.querySelector('[name=profile-form]');
-const addForm = document.querySelector('[name=add-form]');
+const placeTemplate = document.querySelector('#place-template').content;
+
+const popupProfileForm = document.querySelector('[name=profile-form]');
+const popupAddPhotoForm = document.querySelector('[name=add-form]');
 const userNameForm = document.querySelector('.popup__input_type_user-name');
 const userDescForm = document.querySelector('.popup__input_type_user-desc');
 const placeNameForm = document.querySelector('.popup__input_type_place-name');
@@ -8,20 +10,20 @@ const placeLinkForm = document.querySelector('.popup__input_type_place-link');
 const userName = document.querySelector('.profile__user-name');
 const userDesc = document.querySelector('.profile__user-description');
 
-const editButton = document.querySelector('.profile__edit-button');
-const addButton = document.querySelector('.profile__add-button');
+const profileEditButton = document.querySelector('.profile__edit-button');
+const photoAddButton = document.querySelector('.profile__add-button');
 const popupAddPlace = document.querySelector('.popup_type_add-place');
 const popupEditProfile = document.querySelector('.popup_type_edit-profile');
 const popupPhotoView = document.querySelector('.popup_type_photo-view');
 
-const closeAddPlaceButton = popupAddPlace.querySelector('.popup__close-btn');
-const closeEditProfileButton = popupEditProfile.querySelector('.popup__close-btn');
-const closePhotoButton = popupPhotoView.querySelector('.popup__close-btn');
+const buttonCloseAddPlace = popupAddPlace.querySelector('.popup__close-btn');
+const buttonCloseEditProfile = popupEditProfile.querySelector('.popup__close-btn');
+const buttonClosePhotoView = popupPhotoView.querySelector('.popup__close-btn');
 
 const places = document.querySelector('.places');
 
-const popupPhoto = document.querySelector('.popup__photo');
-const popupPhotoTitle = document.querySelector('.popup__photo-title');
+const popupPhotoViewPhoto = document.querySelector('.popup__photo');
+const popupPhotoViewTitle = document.querySelector('.popup__photo-title');
 
 const initialCards = [
     {
@@ -57,29 +59,32 @@ const initialCards = [
 
 /* Открытие попапов */
 
+function openPopup(popup) {
+    popup.classList.add('popup_opened');
+}
 
 function openProfilePopup() {
     userNameForm.value = userName.textContent;
     userDescForm.value = userDesc.textContent;
-    popupEditProfile.classList.add('popup_opened');
+    openPopup(popupEditProfile);
 };
 
 function openAddPopup(){
     placeNameForm.value = '';
     placeLinkForm.value = '';
-    popupAddPlace.classList.add('popup_opened');
+    openPopup(popupAddPlace);
 };
 
 function openPhotoPopup(evt) {
-    popupPhoto.setAttribute('src', evt.target.getAttribute('src'));
-    const photoTitleElement = evt.target.parentElement.querySelector('.place__name').textContent;
-    popupPhoto.setAttribute('alt', photoTitleElement);
-    popupPhotoTitle.textContent = photoTitleElement;
-    popupPhotoView.classList.add('popup_opened');
+    popupPhotoViewPhoto.setAttribute('src', evt.target.getAttribute('src'));
+    const photoTitleText = evt.target.parentElement.querySelector('.place__name').textContent;
+    popupPhotoViewPhoto.setAttribute('alt', photoTitleText);
+    popupPhotoViewTitle.textContent = photoTitleText;
+    openPopup(popupPhotoView);
 };
 
-editButton.addEventListener('click', openProfilePopup);
-addButton.addEventListener('click', openAddPopup);
+profileEditButton.addEventListener('click', openProfilePopup);
+photoAddButton.addEventListener('click', openAddPopup);
 
 /* Закрытие попапов */
 
@@ -87,9 +92,9 @@ function closePopup(popup) {
     popup.classList.remove('popup_opened');
 };
 
-closeEditProfileButton.addEventListener('click', () => closePopup(popupEditProfile));
-closeAddPlaceButton.addEventListener('click', () => closePopup(popupAddPlace));
-closePhotoButton.addEventListener('click', () => closePopup(popupPhotoView));
+buttonCloseEditProfile.addEventListener('click', () => closePopup(popupEditProfile));
+buttonCloseAddPlace.addEventListener('click', () => closePopup(popupAddPlace));
+buttonClosePhotoView.addEventListener('click', () => closePopup(popupPhotoView));
 
 /* Изменение профиля */
 
@@ -100,39 +105,42 @@ function editProfile(evt) {
     closePopup(popupEditProfile);
 };
 
-profileForm.addEventListener('submit', editProfile);
+popupProfileForm.addEventListener('submit', editProfile);
 
 /* Добавление карточек */
 
-function addCard(name, link) {
-    const placeTemplate = document.querySelector('#place-template').content;
+function createCard(name, link) {
     const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
     const placePic = placeElement.querySelector('.place__pic');
     const placeName = placeElement.querySelector('.place__name');
     placeElement.querySelector('.place__like').addEventListener('click', toggleActiveLike);
     placeElement.querySelector('.place__delete-button').addEventListener('click', deleteCard);
-    placeElement.querySelector('.place__pic').addEventListener('click', openPhotoPopup);
+    placePic.addEventListener('click', openPhotoPopup);
 
     placePic.setAttribute('src', link);
     placePic.setAttribute('alt', name);
     placeName.textContent = name;
+    return placeElement;
+}
 
-    places.prepend(placeElement);
-};
+function renderCard(element) {
+    places.prepend(element);
+}
+
 
 function addInitialCards() {
-    initialCards.forEach(elem => addCard(elem.name, elem.link));
+    initialCards.forEach(elem => renderCard(createCard(elem.name, elem.link)));
 };
 
 addInitialCards();
 
 function addPlace(evt) {
     evt.preventDefault();
-    addCard(placeNameForm.value, placeLinkForm.value);
+    renderCard(createCard(placeNameForm.value, placeLinkForm.value));
     closePopup(popupAddPlace);
 }
 
-addForm.addEventListener('submit', addPlace);
+popupAddPhotoForm.addEventListener('submit', addPlace);
 
 /* Лайки 🐶*/
 function toggleActiveLike(evt) {
